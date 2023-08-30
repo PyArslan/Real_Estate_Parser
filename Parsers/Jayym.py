1ba9470f708b11831293b47411ad26e8a974ef55
Arslan <9arslan4work6@gmail.com>
2023-08-30 16:01:55 +0500

    Successfuly tested: Parse cards, links, Save TODO: Loop for parsing links

--- Parsers/Jayym.py
+++ Parsers/Jayym.py
@@ -9,8 +9,23 @@ class Jayym:
         self.Save = Save
         self.Find.get("https://jayym.com/properties.html")
         
+
+    @staticmethod
+    def changekey(array, keys):
+        for i in keys:
+            try:
+                if "Площадь" in i[1]:
+                    array[i[0]] = array.pop(i[1])[:-2].strip()
+                else:
+                    array[i[0]] = array.pop(i[1])
+            except KeyError:
+                pass
+
+        return array
+
     def parse_links(self):
-        card_list = self.Find.xs("//section[@id='listings']/a")
+        """ TODO: Loop """
+        card_list = self.Find.xs("//section[@id='listings']/article/div[2]/a")
 
         link_list = []
 
@@ -20,8 +35,64 @@ class Jayym:
         self.Save.links(link_list, "Jayym")
 
     def parse_cards(self):
-        pass
+        """ TODO: increase count save """
+        with open(f"Parse_Files\\Links_Jayym.txt", "r", encoding="utf8") as file:
+            link_list = file.readline().split(",")[:-1]
+            file.close()
+
+        estate_list = []
+
+        for count in range(len(link_list)):
+            if count % 1000 == 0 and count != 0:
+                self.Save.to_xlsx(estate_list, "Jayym", count)
+                self.Save.links(link_list, "Jayym")
+
+
+            link = link_list.pop(0)
+            print(f"{count+1}. {link}")
+
+            try:
+                self.Find.get(link)
+            except self.Find.WE:
+                print("[Ошибка] Не удалось подключиться. Попробуёте зайти на сайт вручную, если получится то обратитесь к разработчику, если нет - проблема на самом сайте")
+
+            keys = [i.text for i in self.Find.xs("//div[@class='listing-fields']//span") if i.text != ""]
+            values = [i.text for i in self.Find.xs("//div[@class='listing-fields']//div[@class='value']")]
+            info = dict(zip(keys,values))
+
+            info['Наименование'] = self.Find.x("//div[@class='row listing-header']/h1").text
+
+            phone = self.Find.x("//div[@id='df_field_phone']/div[2]/a").text
+            info['Конт.номер'] = "+" + ''.join([i for i in phone if i.isdigit()])
+            
+            price = self.Find.x("//*[@id='df_field_price']/span").text
+            info['Цена предложения ТМТ'] = ''.join([i for i in price if i.isdigit()])
+
+            info['Web ссылка'] = link
+
+            keys = [
+                ['Этажность дома', 'Этажность здания'],
+                ['Дата публикации', 'Добавлено'],
+                ['На каком этаже', 'Этаж'],
+                ['Общая площадь', 'Площадь'],
+                ['Цена предложения ТМТ', 'Цена'],
+                ['Количество комнат', 'Комнаты'],
+                ['Состояние ремонта', 'Ремонт'],
+                ['Местоположение', 'Город'],
+                ['Адрес', 'Район'],
+                ['Цель', 'Недвижимость.'],
+                ['Примечание', 'Описание']
+            ]
+
+            info = self.changekey(info, keys)
+
+            try:
+                info['Цена за 1 кв.метр'] = str(round(float(info['Цена предложения ТМТ']) / float(info['Общая площадь']), 2))
+            except KeyError:
+                pass
+        
+            estate_list.append(info)
 
 
 if __name__ == "__main__":
-    Jayym(Find, Save)
+    Jayym(Find, Save).parse_cards()
